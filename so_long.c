5dd6e190860b5bf3f51492a552cf61ef838d01a2
Antoine BIERSOHN <abiersoh@e2r4p15.clusters.42paris.fr>
2021-12-15 13:33:43 +0100

    Progressing

--- so_long.c
+++ so_long.c
@@ -10,7 +10,7 @@
 #define LEFT_ARROW_KEY 65361
 #define RIGHT_ARROW_KEY 65363
 #define DOWN_ARROW_KEY 65364
-#define A_KEY 7
+#define A_KEY 97
 #define W_KEY 119
 #define Z_KEY 122
 #define Q_KEY 113
@@ -18,11 +18,53 @@
 #define D_KEY 100
 #define ESCAPE_KEY 65307
 
+static int	ft_strlen(const char *str)
+{
+	int	i;
 
- typedef struct	s_vars
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+static char	*ft_strdup(const char *src)
+{
+	int	i;
+	char	*dest;
+
+	dest = malloc(sizeof(char) * ft_strlen(src));
+	if (!dest)
+		return (NULL);
+	i = 0;
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+	
+}
+
+typedef struct s_map
+{
+	int	fd;
+	int	height;
+	int	length;
+	int	nb_exit;
+	int	nb_player;
+	int	nb_collec;
+	char	*map[21];
+	char	**src_images;
+}	t_map;
+
+typedef struct	s_vars
 {
 	void	*mlx;
 	void	*win;
+	int		collected;
+	int		nb_moves;
+	t_map	map;
 }	t_vars;
 
 typedef struct s_img
@@ -43,22 +85,145 @@ typedef struct s_data
 }	t_data;
 
 
+void	ft_update_map(t_vars *vars)
+{
+	int	i;
+	int	j;
+	void	*img;
+	int	img_width;
+	int	img_height;
+
+	img = NULL;
+	i = 0;
+	j = 0;
+	while (vars->map.map[i])
+	{
+		while (vars->map.map[i][j] != '\n')
+		{
+			if (vars->map.map[i][j] == '0')
+				img = mlx_xpm_file_to_image(vars->mlx, "Sol1.xpm", &img_width, &img_height);
+			if (vars->map.map[i][j] == '1')
+				img = mlx_xpm_file_to_image(vars->mlx, "Obstacle1.xpm", &img_width, &img_height);
+			if (vars->map.map[i][j] == 'E')
+				img = mlx_xpm_file_to_image(vars->mlx, "Exit1.xpm", &img_width, &img_height);
+			if (vars->map.map[i][j] == 'P')
+				img = mlx_xpm_file_to_image(vars->mlx, "Personnage1.xpm", &img_width, &img_height);
+			if (vars->map.map[i][j] == 'C')
+				img = mlx_xpm_file_to_image(vars->mlx, "Collectible1.xpm", &img_width, &img_height);
+			mlx_put_image_to_window(vars->mlx, vars->win, img, j * img_width, i * img_height);
+			j++;
+		}
+		i++;
+		j = 0;
+	}
+}
+
 void	ft_mlx_close(t_vars *vars)
 {
 	mlx_destroy_window(vars->mlx, vars->win);
 	exit(EXIT_SUCCESS);
 }
 
+void	get_coords(t_vars *vars, int *x, int *y)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (vars->map.map[i])
+	{
+		while (vars->map.map[i][j])
+		{
+			if (vars->map.map[i][j] == 'P')
+			{
+				*x = j;
+				*y = i;
+				return ;
+			}
+			j++;
+		}
+		i++;
+		j = 0;
+	}
+	*x = 0;
+	*y = 0;
+	printf("Perso non trouve\n");
+}
+
+void	ft_cswitch(char *a, char *b)
+{
+	char	tmp;
+
+	tmp = *a;
+	printf("switching %c & %c\n", *a, *b);
+	*a = *b;
+	*b = tmp;
+}
+
+int	is_accessible(char c, t_vars vars)
+{
+	if (c == '1' || (c == 'E' && vars.collected != vars.map.nb_collec))	//update avec counter de collectibles
+		return (0);
+	return (1);
+}
+
+void	ft_move(t_vars *vars, int keycode)
+{
+	int	x;
+	int	y;
+	get_coords(vars, &x, &y);
+	if (keycode == W_KEY && is_accessible(vars->map.map[y - 1][x], *vars))	//update avec exit et collectibles.
+	{
+		vars->collected += (vars->map.map[y - 1][x] == 'C');
+		vars->nb_moves++;
+		if (vars->map.map[y - 1][x] == 'E')
+			ft_mlx_close(vars);
+		vars->map.map[y][x] = '0';	
+		vars->map.map[y - 1][x] = 'P';	
+	}
+	else if (keycode == S_KEY && is_accessible(vars->map.map[y + 1][x], *vars))	//update avec exit et collectibles.
+	{
+		vars->collected += (vars->map.map[y + 1][x] == 'C');
+		vars->nb_moves++;
+		if (vars->map.map[y + 1][x] == 'E')
+			ft_mlx_close(vars);
+		vars->map.map[y][x] = '0';	
+		vars->map.map[y + 1][x] = 'P';	
+	}
+	else if (keycode == D_KEY && is_accessible(vars->map.map[y][x + 1], *vars))	//update avec exit et collectibles.
+	{
+		vars->collected += (vars->map.map[y][x + 1] == 'C');
+		vars->nb_moves++;
+		if (vars->map.map[y][x + 1] == 'E')
+			ft_mlx_close(vars);
+		vars->map.map[y][x] = '0';	
+		vars->map.map[y][x + 1] = 'P';	
+	}
+	else if (keycode == A_KEY && is_accessible(vars->map.map[y][x - 1], *vars))	//update avec exit et collectibles.
+	{
+		vars->collected += (vars->map.map[y][x - 1] == 'C');
+		vars->nb_moves++;
+		if (vars->map.map[y][x - 1] == 'E')
+			ft_mlx_close(vars);
+		vars->map.map[y][x] = '0';
+		vars->map.map[y][x - 1] = 'P';
+	}
+	printf("collectibles : %d / %d\nMoves : %d\n", vars->collected, vars->map.nb_collec, vars->nb_moves);
+}
 
 int	key_hook(int keycode, t_vars *vars)
 {
 	
 	if (keycode == 65307)
 		ft_mlx_close(vars);
-	else if (keycode == UP_ARROW_KEY)
-	return(printf("Hello from key_hook %d!\n", keycode));
+	if (keycode == W_KEY || keycode == A_KEY || keycode == S_KEY || keycode == D_KEY)
+	{
+		ft_move(vars, keycode);
+		ft_update_map(vars);
+	}
+	return(keycode);
 }
-
 
 static	char	*ft_strchr(char *str, char c)
 {
@@ -68,26 +233,6 @@ static	char	*ft_strchr(char *str, char c)
 	return (NULL);
 }
 
-static int	ft_strlen(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i])
-		i++;
-	return (i);
-}
-
-typedef struct s_map
-{
-	int	fd;
-	int	height;
-	int	length;
-	int	nb_exit;
-	int	nb_player;
-	int	nb_collec;
-	char	*map[21];
-}	t_map;
 
 int	ft_check_line(t_map *map, int i)
 {
@@ -163,167 +308,89 @@ int	ft_count_lines(t_map map)
 	return (i);
 }
 
-void	ft_display_map(t_map map)
+
+void	ft_display_map(t_vars *vars)
 {
 	int	i;
 	int	j;
 	void	*img;
 	int	img_width;
 	int	img_height;
-	t_vars	vars;
+//	t_vars	vars;
 
+	img = NULL;
 	i = 0;
 	j = 0;
-	int	k = 0;
-	vars.mlx = mlx_init();
-	vars.win = mlx_new_window(vars.mlx, 50 * map.length, 50 * map.height, "Thanks for all the fish !");
-	while (k < 10000)
-	{	
-		while (map.map[i])
+	vars->mlx = mlx_init();
+	vars->win = mlx_new_window(vars->mlx, 50 * vars->map.length, 50 * vars->map.height, "Thanks for all the fish !");
+	while (vars->map.map[i])
+	{
+		while (vars->map.map[i][j] != '\n')
 		{
-			while (map.map[i][j] != '\n')
-			{
-				if (map.map[i][j] == '0')
-				{
-					if (k % 4 == 0)
-						img = mlx_xpm_file_to_image(vars.mlx, "Sol1.xpm", &img_width, &img_height);
-					if (k % 4 == 1)
-						img = mlx_xpm_file_to_image(vars.mlx, "Sol2.xpm", &img_width, &img_height);
-					if (k % 4 == 2)
-						img = mlx_xpm_file_to_image(vars.mlx, "Sol3.xpm", &img_width, &img_height);
-					if (k % 4 == 3)
-						img = mlx_xpm_file_to_image(vars.mlx, "Sol4.xpm", &img_width, &img_height);
-				}
-				if (map.map[i][j] == '1')
-					img = mlx_xpm_file_to_image(vars.mlx, "Obstacle1.xpm", &img_width, &img_height);
-				if (map.map[i][j] == 'E')
-					img = mlx_xpm_file_to_image(vars.mlx, "Exit1.xpm", &img_width, &img_height);
-				if (map.map[i][j] == 'P')
-					img = mlx_xpm_file_to_image(vars.mlx, "Personnage1.xpm", &img_width, &img_height);
-				if (map.map[i][j] == 'C')
-					img = mlx_xpm_file_to_image(vars.mlx, "Collectible1.xpm", &img_width, &img_height);
-				mlx_put_image_to_window(vars.mlx, vars.win, img, j * img_width, i * img_height);
-				j++;
-			}
-			i++;
-			j = 0;
+			if (vars->map.map[i][j] == '0')
+				img = mlx_xpm_file_to_image(vars->mlx, "Sol1.xpm", &img_width, &img_height);
+			if (vars->map.map[i][j] == '1')
+				img = mlx_xpm_file_to_image(vars->mlx, "Obstacle1.xpm", &img_width, &img_height);
+			if (vars->map.map[i][j] == 'E')
+				img = mlx_xpm_file_to_image(vars->mlx, "Exit1.xpm", &img_width, &img_height);
+			if (vars->map.map[i][j] == 'P')
+				img = mlx_xpm_file_to_image(vars->mlx, "Personnage1.xpm", &img_width, &img_height);
+			if (vars->map.map[i][j] == 'C')
+				img = mlx_xpm_file_to_image(vars->mlx, "Collectible1.xpm", &img_width, &img_height);
+			mlx_put_image_to_window(vars->mlx, vars->win, img, j * img_width, i * img_height);
+			j++;
 		}
-		i = 0;
-		k++;
-		usleep(250000);
+		i++;
+		j = 0;
 	}
-	mlx_loop(vars.mlx);
+	mlx_key_hook(vars->win, key_hook, vars);
+	mlx_loop(vars->mlx);
 }
 
+char	**ft_init_images(void)
+{
+	char	**dest;
+
+	dest = malloc(sizeof(char *) * 6);
+	if (!dest)
+		return (NULL);
+	dest[0] = ft_strdup("Sol1.xpm");
+	dest[1] = ft_strdup("Obstacle1.xpm");
+	dest[2] = ft_strdup("Collectible1.xpm");
+	dest[3] = ft_strdup("Personnage1.xpm");
+	dest[4] = ft_strdup("Exit1.xpm");
+	dest[5] = NULL;
+	return(dest);
+}
+
+void	ft_init_vars(t_vars *vars)
+{
+
+	vars->map.length = 0;
+	vars->map.height = 0;
+	vars->map.nb_exit = 0;
+	vars->map.nb_collec = 0;
+	vars->map.nb_player = 0;
+	vars->map.fd = 0;
+	vars->map.src_images = ft_init_images();
+	vars->collected = 0;
+	vars->nb_moves = 0;
+}
 int main(int ac, char **av)
 {
-//	t_vars	vars;
-//	t_img	img;
-//	t_data	data;
-	t_map	map;
+	t_vars	vars;
 
-
-	map.length = 0;
-	map.height = 0;
-	map.nb_exit = 0;
-	map.nb_collec = 0;
-	map.nb_player = 0;
-	map.fd = 0;
-	
+	ft_init_vars(&vars);
 	if (ac != 2)
 		return(printf("Mauvais nombre d'arguments\n"), -1);
-	map.fd = open(av[1], O_RDONLY);
-	if (map.fd <= 0)
+	vars.map.fd = open(av[1], O_RDONLY);
+	if (vars.map.fd <= 0)
 		return(printf("Erreur lors de l'ouverture du fichier"), -1);
-	map.height = ft_count_lines(map);
-	close(map.fd);
-	map.fd = open(av[1], O_RDONLY);
-	if (ft_init_map(&map) == -1)
+	vars.map.height = ft_count_lines(vars.map);
+	close(vars.map.fd);
+	vars.map.fd = open(av[1], O_RDONLY);
+	if (ft_init_map(&vars.map) == -1)
 		return(-1);
 	printf("Map valide !\n");
-	int	i = 0;
-	while (map.map[i])
-	{
-		printf("%s", map.map[i++]);
-	}
-	printf("hauteur, largeur : %d, %d\n", map.height, map.length);
-	printf("Exit : %d, Player : %d, collec : %d\n", map.nb_exit, map.nb_player, map.nb_collec);
-	ft_display_map(map);
-
+	ft_display_map(&vars);
 }
-
-
-
-
-
-	/*vars.mlx = mlx_init();
-	vars.win = mlx_new_window(mlx, 640, 480, "Penis");
-	img = mlx_xpm_file_to_image(mlx, relative_path, &img_width, &img_height);
-	if (img != NULL)
-	{
-		while (i * img_width < 640)
-		{
-			while (j * img_height < 480)
-			{
-				mlx_put_image_to_window(mlx, win, img, i * img_width, j * img_height);
-				j++;
-				usleep(100000);
-			}
-			i++;
-			j = 0;
-		}
-	}
-	mlx_loop(mlx);
-}*/
-
-
-
-/*int main(void)
-{
-	t_vars	vars;
-	t_data img;
-
-	vars.mlx = mlx_init();
-	img.img = mlx_new_image(vars.mlx, 640, 480);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
-	vars.win = mlx_new_window(vars.mlx, 640, 480, "Hello World!");
-	mlx_key_hook(vars.win, key_hook, &vars);
-	mlx_loop(vars.mlx);
-}*/
-
-
-/*int	main(void)
-{
-	void	*mlx;
-	void	*mlx_win;
-	t_data	img;
-
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 800, 600, "Hello world!");
-	img.img = mlx_new_image(mlx, 800, 600);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
-	ft_put_square(&img, 5, 5, 50, 0x00FF0000);
-	ft_put_rainbow(&img, 100, 100, 100);
-	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
-	mlx_loop(mlx);
-
-}*/
-
-
-/*
-int	main(void)
-{
-	t_data	img;
-	void	*mlx;
-	void	*mlx_win;
-
-	mlx = mlx_init();
-	img.img = mlx_new_image(mlx, WPIX, HPIX);
-
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img_endian);
-	
-	//mlx_win = mlx_new_window(mlx, WPIX, HPIX, "Hello world!");
-	//mlx_loop(mlx);
-
-	
-}*/
